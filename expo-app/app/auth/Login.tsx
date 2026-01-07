52df39846c0dd34b0aa779e355f395137366f290
Vinay Gupta <vinay@seekware.in>
2026-01-08 00:03:42 +0530

    pushed changes

--- expo-app/app/auth/Login.tsx
+++ expo-app/app/auth/Login.tsx
@@ -87,9 +87,6 @@ const LoginScreen = () => {
         apiResponse?.data.employerId && await AsyncStorage.setItem("employerId", apiResponse?.data.employerId);
         const storedData = await AsyncStorage.getItem('userDetails');
 
-        if (apiResponse?.data.employerId) return router.replace('/home');
-
-        // const storedData = await AsyncStorage.getItem('userDetails');
         const user = JSON.parse(storedData ?? "");
 
         console.log("Stored user data:", storedData);
