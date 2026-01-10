a80c906f494faf821eb524b57104f215bafe446a
Prince Singh <princesingh26lmp@gmail.com>
2026-01-10 12:22:01 +0530

    fix:latestcode...

--- expo-app/src/screens/home/FeatureFrame.tsx
+++ expo-app/src/screens/home/FeatureFrame.tsx
@@ -80,8 +80,7 @@ const FeatureFrame: React.FC<FeatureFrameProps> = ({ onOpenPDF }) => {
             icon: ImagesAssets.GlobeLogo,
             title: t("social"),
             description: t("social_description"),
-            // onPress: () => router.push("/(bottomtab)/(community)/social"),
-             onPress: () => router.push("/personalitymap/PersonalityMapTestScreen"),
+            onPress: () => router.push("/(bottomtab)/(community)/social"),
         },
         {
             icon: ImagesAssets.UsersLogo,
